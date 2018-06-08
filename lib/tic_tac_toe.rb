69cd762939c4395968b86acd0fd709e019591255
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-08 00:52:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  
+
   # Define your WIN_COMBINATIONS constant
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -14,34 +14,34 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [6,4,2]]
-    
+
   def play
     until over?
       turn
     end
-    
+
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
   end
-  
+
   #current player method
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
- 
+
   #turn count method
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
- 
+
  #position_taken method
  def position_taken?(index)
    !(@board[index].nil? || @board[index] == " ")
   end
-  
+
  # code your #valid_move? method here
  def valid_move?(index)
    if index.between?(0,8)
@@ -78,7 +78,7 @@ def turn
   display_board
 end
 
- 
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -86,13 +86,13 @@ end
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-   
+
    def won?
      board_empty = @board.none? { |i| i == "X" || i = "O"}
     if board_empty
       false
-    else 
-      WIN_COMBINATIONS.each do |combo| 
+    else
+      WIN_COMBINATIONS.each do |combo|
         if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
           return combo
         end
@@ -100,18 +100,18 @@ end
       return false
     end
   end
-  
+
   #full method
   def full?
     @board.all? { |i| i =="X" || i == "O"}
   end
-  
-  
+
+
   #draw method
  def draw?
    !won? && full? ? true : false
   end
- 
+
   #over method
   def over?
     won? || draw? || full? ? true : false
